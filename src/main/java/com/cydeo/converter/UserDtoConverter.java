0514c5926ed212866f5f912626b6612ca41d1704
efecagrisen <efecagrisen@yahoo.com>
2024-07-25 17:18:24 +0300

    repository and service layer added

--- src/main/java/com/cydeo/converter/UserDtoConverter.java
+++ src/main/java/com/cydeo/converter/UserDtoConverter.java
@@ -1,14 +1,15 @@
 package com.cydeo.converter;
 
 import com.cydeo.dto.UserDTO;
-import com.cydeo.service.UserService;
 //import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import com.cydeo.service.UserService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 //@ConfigurationPropertiesBinding
 public class UserDtoConverter implements Converter<String, UserDTO> {
+
 
     UserService userService;
 
@@ -23,7 +24,7 @@ public class UserDtoConverter implements Converter<String, UserDTO> {
             return null;
         }
 
-        return userService.findById(source);
+        return userService.findByUserName(source);
 
     }
 

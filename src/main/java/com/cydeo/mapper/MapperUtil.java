d1b0d63ecd18b15fa1b59157ea67e20b69077017
efecagrisen <efecagrisen@yahoo.com>
2024-07-26 11:41:18 +0300

    User save service completed

---
+++ src/main/java/com/cydeo/mapper/MapperUtil.java
@@ -0,0 +1,21 @@
+package com.cydeo.mapper;
+
+import org.modelmapper.ModelMapper;
+
+public class MapperUtil {
+
+    private final ModelMapper modelMapper;
+
+    public MapperUtil(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public <E, D> E convertToEntity(D dto, Class<E> entityClass){
+       return modelMapper.map(dto,entityClass);
+    }
+
+    public <D, E> D convertToDto(E entity, Class<D> dtoClass){
+        return modelMapper.map(entity,dtoClass);
+    }
+
+}
